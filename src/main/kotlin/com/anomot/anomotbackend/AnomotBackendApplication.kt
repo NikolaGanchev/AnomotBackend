4a8cd33f45fcf16344cd1153841553af1372f3e3
Nikola Ganchev <nikolamihailovg@gmail.com>
2022-09-07 17:18:12 +0300

    Initial commit

---
+++ src/main/kotlin/com/anomot/anomotbackend/AnomotBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.anomot.anomotbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AnomotBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<AnomotBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/anomot/anomotbackend/AnomotBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.anomot.anomotbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AnomotBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
