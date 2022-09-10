b3eccb7812b71146cbd8c064423d7a82efbb5b92
Nikola Ganchev <nikolamihailovg@gmail.com>
2022-09-10 14:33:23 +0300

    Remove useless validation annotations

--- src/main/kotlin/com/anomot/anomotbackend/entities/User.kt
+++ src/main/kotlin/com/anomot/anomotbackend/entities/User.kt
@@ -1,17 +1,13 @@
 package com.anomot.anomotbackend.entities
 
 import javax.persistence.*
-import javax.validation.constraints.Email
-import javax.validation.constraints.Size
 
 
 @Entity
 @Table(name="users")
 class User(
-    @Email(regexp ="[a-z0-9!#$%&'*+/=?^_`{|}~.-]+@[a-z0-9-]+(\\.[a-z0-9-]+)*", message = "Email is not valid")
-    @Size(max=254, message = "Email is too long")
     @Column(unique = true)
     var email: String,
     var password: String,
-    @Size(max = 40, message = "Username is too long") var username: String,
+    var username: String,
     @Id @GeneratedValue(strategy = GenerationType.AUTO) var id: Long? = null)
