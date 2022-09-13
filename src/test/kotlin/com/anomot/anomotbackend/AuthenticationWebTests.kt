20a74489d1fc4ad3ba9ec67f8f6647b83a2efac2
Nikola Ganchev <nikolamihailovg@gmail.com>
2022-09-13 21:50:36 +0300

    Add email verify web tests

--- src/test/kotlin/com/anomot/anomotbackend/AuthenticationWebTests.kt
+++ src/test/kotlin/com/anomot/anomotbackend/AuthenticationWebTests.kt
@@ -1,14 +1,17 @@
 package com.anomot.anomotbackend
 
 import com.anomot.anomotbackend.controllers.AuthController
+import com.anomot.anomotbackend.dto.EmailVerifyDto
 import com.anomot.anomotbackend.dto.UserDto
 import com.anomot.anomotbackend.dto.UserRegisterDto
 import com.anomot.anomotbackend.entities.Authority
+import com.anomot.anomotbackend.entities.EmailVerificationToken
 import com.anomot.anomotbackend.entities.User
 import com.anomot.anomotbackend.exceptions.UserAlreadyExistsException
 import com.anomot.anomotbackend.security.Authorities
 import com.anomot.anomotbackend.security.CustomUserDetails
 import com.anomot.anomotbackend.security.WebSecurityConfig
+import com.anomot.anomotbackend.services.EmailVerificationService
 import com.anomot.anomotbackend.services.UserDetailsServiceImpl
 import com.anomot.anomotbackend.utils.Constants
 import com.ninjasquad.springmockk.MockkBean
@@ -30,6 +33,8 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.web.context.WebApplicationContext
+import java.time.Instant
+import java.util.*
 
 @ContextConfiguration(classes = [WebSecurityConfig::class])
 @Import(AuthController::class)
@@ -43,6 +48,8 @@ class AuthenticationWebTests @Autowired constructor(
 
     @MockkBean
     private lateinit var userDetailsServiceImpl: UserDetailsServiceImpl
+    @MockkBean
+    private lateinit var emailVerificationService: EmailVerificationService
 
     @BeforeAll
     fun setup() {
@@ -57,8 +64,18 @@ class AuthenticationWebTests @Autowired constructor(
         val authority = Authority(Authorities.USER.roleName)
         val user = UserRegisterDto("example@test.com", "password12$", "Georgi")
         val expectedResult = UserDto("example@test.com", "Georgi", mutableListOf(authority.authority))
+        val emailVerificationToken = EmailVerificationToken(
+                "code",
+                User("example@test.com", "password12$", "Georgi", mutableListOf(authority)),
+                Date.from(Instant.now()))
+
 
         every { userDetailsServiceImpl.createUser(user) } returns expectedResult
+        every { emailVerificationService.generateVerificationCode() } returns "code"
+        every { emailVerificationService.createEmailVerificationToken(any(), any(), any()) } returns emailVerificationToken
+        every { emailVerificationService.generateExpiryDate(any(), any()) } returns Date.from(Instant.now())
+        every { emailVerificationService.sendVerificationEmail(any(), any()) }
+        every { emailVerificationService.saveEmailVerificationToken(any()) } returns emailVerificationToken
 
         mockMvc.perform(post("/account/new")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -84,12 +101,12 @@ class AuthenticationWebTests @Autowired constructor(
     @Test
     fun `When user login then return 200 and user`() {
         val authority = Authority(Authorities.USER.roleName)
-        val user = User("example@test.com", "password12$", "Georgi", mutableListOf(authority))
+        val user = User("example@test.com", "password12$", "Georgi", mutableListOf(authority), true)
 
         val dbUser = User("example@test.com",
                 passwordEncoder.encode("password12$"),
                 "Georgi",
-                mutableListOf(authority))
+                mutableListOf(authority), true)
 
         val expectedUserDetails = CustomUserDetails(dbUser)
 
@@ -108,7 +125,7 @@ class AuthenticationWebTests @Autowired constructor(
     @Test
     fun `When user login with bad credentials then return 403`() {
         val authority = Authority(Authorities.USER.roleName)
-        val user = User("example@test.com", "password12$", "Georgi", mutableListOf(authority))
+        val user = User("example@test.com", "password12$", "Georgi", mutableListOf(authority), true)
 
         every { userDetailsServiceImpl.loadUserByUsername(any()) } throws UsernameNotFoundException("Email not found")
 
@@ -118,5 +135,29 @@ class AuthenticationWebTests @Autowired constructor(
                 .andExpect(status().isForbidden)
                 .andExpect(content().contentType(MediaType.TEXT_PLAIN))
                 .andExpect(content().string("Login error"))
+    }
+
+    @Test
+    fun `When verify email code valid then return 201`() {
+        val token = EmailVerifyDto("code")
+
+        every { emailVerificationService.verifyEmail("code", any()) } returns true
+
+        mockMvc.perform(post("/account/email/verify")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(TestUtils.objectToJSON(token)))
+                .andExpect(status().isCreated)
+    }
+
+    @Test
+    fun `When verify email code invalid then return 403`() {
+        val token = EmailVerifyDto("code")
+
+        every { emailVerificationService.verifyEmail("code", any()) } returns false
+
+        mockMvc.perform(post("/account/email/verify")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(TestUtils.objectToJSON(token)))
+                .andExpect(status().isForbidden)
     }
 }
