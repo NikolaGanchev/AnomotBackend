7c9682af3723575642bcdc0d1ede871ef4b37786
Nikola Ganchev <nikolamihailovg@gmail.com>
2022-09-24 12:37:45 +0300

    Remove unneeded email sent exception

--- src/main/kotlin/com/anomot/anomotbackend/exceptions/MfaEmailSentException.kt
+++
@@ -1,6 +0,0 @@
-package com.anomot.anomotbackend.exceptions
-
-import org.springframework.security.core.AuthenticationException
-
-class MfaEmailSentException(message: String): AuthenticationException(message) {
-}
