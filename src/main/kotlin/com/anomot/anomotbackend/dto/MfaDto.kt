d3ffe3dd75ceae5358d0b72ae767efecc2925f23
Nikola Ganchev <nikolamihailovg@gmail.com>
2022-10-08 17:50:25 +0300

    Fix bug

--- src/main/kotlin/com/anomot/anomotbackend/dto/MfaDto.kt
+++ src/main/kotlin/com/anomot/anomotbackend/dto/MfaDto.kt
@@ -4,10 +4,10 @@ import com.anomot.anomotbackend.utils.Constants
 import javax.validation.constraints.Pattern
 import javax.validation.constraints.Size
 
-class MfaDto(
+data class MfaDto(
         @Size(min = Constants.MFA_PASSWORD_LENGTH, max = Constants.MFA_PASSWORD_LENGTH)
-        code: String?,
+        val code: String?,
         @Pattern(regexp = "totp|email", flags = [Pattern.Flag.CASE_INSENSITIVE])
-        method: String?,
+        val method: String?,
         @Size(min = Constants.MFA_RECOVERY_CODE_LENGTH, max = Constants.MFA_RECOVERY_CODE_LENGTH)
-        recoveryCode: String?)
+        val recoveryCode: String?)
