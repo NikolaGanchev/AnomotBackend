fcdfe8008d91f4919e0990ef0adcee3655a3d194
Nikola Ganchev <nikolamihailovg@gmail.com>
2023-02-11 18:43:09 +0200

    Fix query error

--- src/main/kotlin/com/anomot/anomotbackend/repositories/PostRepository.kt
+++ src/main/kotlin/com/anomot/anomotbackend/repositories/PostRepository.kt
@@ -15,7 +15,7 @@ import java.util.*
 interface PostRepository: JpaRepository<Post, Long> {
 
 
-    @Query("select distinct new com.anomot.anomotbackend.dto.PostWithLikes(p, " +
+    @Query("select new com.anomot.anomotbackend.dto.PostWithLikes(p, " +
             "(select count(l) from Like l where l.post = p), " +
             // TODO could use exists here
             "(select count(l) > 0 from Like l where l.post = p and l.likedBy = ?1)) " +
@@ -30,7 +30,7 @@ interface PostRepository: JpaRepository<Post, Long> {
         there needs to be no battles with the post OR
             the battle is finished OR there exists a vote from the user for the post OR the user is in the battle
      */
-    @Query("select distinct new com.anomot.anomotbackend.dto.PostWithLikes(p, " +
+    @Query("select new com.anomot.anomotbackend.dto.PostWithLikes(p, " +
             "(select count(l) from Like l where l.post = p), " +
             "(select count(l) > 0 from Like l where l.post = p and l.likedBy = ?2)) " +
             "from Post p " +
@@ -64,16 +64,16 @@ interface PostRepository: JpaRepository<Post, Long> {
 
     fun deleteByIdAndPoster(id: Long, poster: User): Long
 
-    @Query("select distinct new com.anomot.anomotbackend.dto.PostWithLikes(p, " +
+    @Query("select new com.anomot.anomotbackend.dto.PostWithLikes(p, " +
             "(select count(l) from Like l where l.post = p), " +
             "(select count(l) > 0 from Like l where l.post = p and l.likedBy = ?1)) " +
-            "from Post p, Follow f where p.poster = f.followed and f.follower = ?1")
+            "from Post p join Follow f on p.poster = f.followed where f.follower = ?1")
     fun getFeed(user: User, pageable: Pageable): List<PostWithLikes>
 
     @Query("select p.poster from Post p where p.id = ?1")
     fun findPosterById(id: Long): User
 
-    @Query("select distinct new com.anomot.anomotbackend.dto.PostWithLikes(p, " +
+    @Query("select new com.anomot.anomotbackend.dto.PostWithLikes(p, " +
             "(select count(l) from Like l where l.post = p), " +
             // TODO could use exists here
             "(select count(l) > 0 from Like l where l.post = p and l.likedBy = ?1)) " +
@@ -84,7 +84,7 @@ interface PostRepository: JpaRepository<Post, Long> {
     @Query("delete from Post p where p.poster = ?1")
     fun deleteByUser(user: User)
 
-    @Query("select distinct new com.anomot.anomotbackend.dto.PostWithLikeNumber(p, " +
+    @Query("select new com.anomot.anomotbackend.dto.PostWithLikeNumber(p, " +
             "(select count(l) from Like l where l.post = p)) " +
             "from Post p where p.poster = ?1")
     fun getAllByPoster(user: User, page: Pageable): List<PostWithLikeNumber>
